dd308128a822501102a2f5dd542a81c78986c405
Christopher Mangan <chrisjm66@gmail.com>
2025-09-02 16:04:15 -0400

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,10 @@
+// Chris Mangan - my first c++ program!
+
+#include <iostream> // allows us to output to the console
+using namespace std; // standard library that provides us with some more useful tools.
+
+int main() {
+	cout << "Hello World!"; // simple print statement
+	return 0; // since main needs to return an int. 0 = no issues, program ended. other nonzero values indicate exceptions.
+	// by default, a return 0 is added by the compiler if omitted.
+}
